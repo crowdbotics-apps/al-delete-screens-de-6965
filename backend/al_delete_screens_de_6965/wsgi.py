e2087d54d6271202530332934414ab8dddd60753
Crowdbotics <team@crowdbotics.com>
2020-07-02 19:18:49 +0000

    Update models

--- backend/al_delete_screens_de_6965/wsgi.py
+++ backend/al_delete_screens_de_6965/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'al_delete_screens_de_6965.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "al_delete_screens_de_6965.settings")
 
 application = get_wsgi_application()
